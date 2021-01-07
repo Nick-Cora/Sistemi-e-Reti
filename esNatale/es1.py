9139890b0be4f451cca65eeefece582ad2addf74
Nick-Cora <72200919+Nick-Cora@users.noreply.github.com>
2021-01-07 11:37:02 +0100

    Add files via upload

--- esNatale/es1.py
+++ esNatale/es1.py
@@ -8,8 +8,7 @@ import random
 
 dictCar = {"1": [48,57], "2": [65,90], "3": [97,122]}
 
-def pwSemplice():
-    dim = 8
+def generaPassword(dim):
     pw = ""
     cnt = 0
     while cnt != dim:
@@ -23,29 +22,17 @@ def pwSemplice():
         cnt = cnt + 1
     print(f"La password è {pw}")
 
-
-def pwCompl():
-    dim = 20
-    pw = ""
-    cnt = 0
-    while cnt != dim:
-        rand = random.randint(1, 3)
-        if rand == 1:
-            pw = pw + chr(random.randint(dictCar["1"][0], dictCar["1"][1]))
-        elif rand == 2:
-            pw = pw + chr(random.randint(dictCar["2"][0], dictCar["2"][1]))
-        elif rand == 3:
-            pw = pw + chr(random.randint(dictCar["3"][0], dictCar["3"][1]))
-        cnt = cnt + 1
-    print(f"La password è {pw}")
 
 
 def main():
     scelta = int(input("1 per password semplice, 2 per password complessa\n"))
     if scelta == 1:
-        pwSemplice()
+        dim = 8
     elif scelta == 2:
-        pwCompl()
+        dim = 20
+    elif scelta != 1 or scheta != 0:
+        raise Exception("ERRORE: scelta errata")    
+    generaPassword(dim)
        
 
 if __name__ == "__main__":
